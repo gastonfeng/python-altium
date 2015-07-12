707f2b51987532c5bd4c3ea7775323e264c4cb97
Martin Panter <vadmium  gmailcom>
2015-07-12 03:29:19 +0000

    More get_int() call sites

--- altium.py
+++ altium.py
@@ -703,9 +703,9 @@ def overline(name):
 
 def polyline(renderer, obj):
     points = list()
-    for location in range(int(obj["LOCATIONCOUNT"])):
+    for location in range(get_int(obj, "LOCATIONCOUNT")):
         location = format(1 + location)
-        points.append(tuple(int(obj[x + location]) for x in "XY"))
+        points.append(tuple(get_int(obj, x + location) for x in "XY"))
     kw = dict(points=points)
     kw.update(colour=colour(obj))
     renderer.polyline(**kw)
